7e9e1a5fadf2db520600a6dbc53ccf61b862aeb0
progToMax <m@m.com>
2019-12-13 16:58:16 -0600

    added more pictures and sections and cards

--- load.js
+++ load.js
@@ -1,5 +1,25 @@
 (function() {
+    const secs = ['johndoe', 'maryallen','davidgreen','paulbunyan','aboutme', 'aboutme2', 'aboutme3', 'morelinks'];
+    for (const i of secs) {
+        const li = document.createElement("li");
+        const bodyy = document.body.getBoundingClientRect().y;
+        const secy = document.querySelector(`[data='${i}']`).getBoundingClientRect().y;
+        const y = secy - bodyy;
+        navs = document.querySelector("nav ul");
+        const anchor = document.createElement("a");
+        anchor.innerHTML = i;
+        li.appendChild(anchor);
 
+        li.addEventListener('click', function() {
+            scrollTo(0, y)
+        });
+        const alldata = document.querySelectorAll(`[data]`);
+        for (j of alldata) {
+            j.removeAttribute('activesec');
+        }
+        document.querySelector(`[data='${i}']`).setAttribute('activesec', 'active');
+        navs.appendChild(li);
+    }
     var x = new XMLHttpRequest();
 
     x.onreadystatechange = (function(e) {
